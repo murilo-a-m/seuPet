b9a63c3a36b37282310ac653f9dd2e7a8e5118e7
Murilo Analiel Machado <muriloam@outlook.com>
2022-05-20 19:43:17 -0300

    Adicionando controle de arquivos

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const fs = require('fs').promises;
+
+async function saveData(data) {
+  try {
+    await fs.writeFile('users.json', JSON.stringify(data));
+  } catch (error) {
+    console.error(`Got an error trying to write to a file: ${error.message}`);
+  }
+}
+
+async function readData(filePath) {
+    try {
+      const data = await fs.readFile(filePath);
+      console.log(data.toString());
+    } catch (error) {
+      console.error(`Got an error trying to read the file: ${error.message}`);
+    }
+}
+
+
+
+
+
+
